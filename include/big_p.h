87fb051b324d8de9ee20c3a1f38fe5840cff3381
cuamckuu <sasha23969@gmail.com>
2018-03-25 13:32:43 +0300

    Polynom header

--- include/big_p.h
+++ include/big_p.h
@@ -2,37 +2,29 @@
 
 class BigPol{
 private:
-    std::vector<BigFra> coefs;
-
+	std::vector<BigFra> coefs;
+	
 public:
-    friend std::ostream& operator<<(std::ostream &stream, const BigPol &pol);
-
-    //=====MODULES=====
-    //Author:
-
-    //======================
-    //==EXTRA MODULES==
-    BigFra calculate(BigFra x); // Calculate polynom value, returns result
-    friend BigPol ADD_PP_P(BigPol lhs, BigPol rhs);
-    friend BigPol MUL_PQ_P(BigPol lhs, BigFra rhs);
-    friend BigPol SUB_PP_P(BigPol lhs, BigPol rhs);
-    friend BigPol MUL_Pxk_P(BigPol lhs, int k);
-    friend BigPol MUL_PP_P(BigPol lhs, BigPol rhs);
-
-    //======Operators support======
-    BigPol operator+(const BigPol &rhs);
-    BigPol operator-(const BigPol &rhs);
-    BigPol operator*(const BigFra &rhs);
-    BigPol operator*(const BigPol &rhs);
-    BigPol operator*(const int &rhs);
-    //BigPol operator*(const BigPol &rhs);
-
-    BigPol operator/(const BigPol &rhs);
-    BigPol operator%(const BigPol &rhs);
-    bool operator==(const BigPol &rhs);
-
-    //=============================
-
-    BigPol(std::string str);
-
+	friend std::ostream& operator<<(std::ostream &stream, const BigFra &pol);
+	
+	//=====MODULES=====
+	//Author: 
+	
+	//======================
+	//==EXTRA MODULES==
+	BigFra calculate(BigFra x); // Calculate polynom value, returns result 
+	
+	//======Operators support======
+	BigPol operator+(const BigPol &rhs);
+	BigPol operator-(const BigPol &rhs);
+	BigPol operator*(const BigPol &rhs);
+	BigPol operator*(const int &rhs);
+	BigPol operator/(const BigPol &rhs);
+	BigPol operator%(const BigPol &rhs);
+	
+	bool operator==(const BigPol &rhs);
+	
+	//=============================
+	
+		
 };
