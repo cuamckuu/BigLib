2d43194fe7a5d4b27c3c7c2aeee4d8b18fe27bf8
cuamckuu <sasha23969@gmail.com>
2018-03-25 14:01:29 +0300

    BigFra class added

--- big_q.cpp
+++ big_q.cpp
@@ -1,19 +1,7 @@
-#include <sstream>
 #include "include/big_q.h"
 
 BigFra::BigFra(BigInt numerator, BigInt denominator): numerator(numerator), denominator(denominator)  {
 	*this = RED_Q_Q(*this);
-}
-
-BigFra::BigFra(std::string str) : numerator("0"), denominator("0") {
-    std::stringstream ss(str);
-
-    BigInt temp("0");
-    while(ss >> temp){
-        numerator = temp;
-        ss >> temp;
-        denominator = temp;
-    }
 }
 
 std::ostream& operator<< (std::ostream &stream, BigFra num){
@@ -21,26 +9,59 @@ std::ostream& operator<< (std::ostream &stream, BigFra num){
 	
     num = RED_Q_Q(num);
     num.normalise();
+    
     stream << num.numerator << "/" << num.denominator;
 
     return stream;
 }
 
-void BigFra::normalise(){
-    int isNegative1 = POZ_Z_D(numerator);
-    int isNegative2 = POZ_Z_D(denominator);
-    if(isNegative1 == isNegative2 || numerator == BigInt("0")){
-        numerator = BigInt(numerator, false);
-        denominator = BigInt(denominator, false);
-    }else{
-        numerator = BigInt(numerator, true);
-        denominator = BigInt(denominator, false);
+bool INT_Q_B(BigFra lhs) {
+    /* Checks if fraction is integer */
+
+    return (lhs.numerator % lhs.denominator == BigInt("0"));
+}
+
+BigFra TRANS_Z_Q(BigInt lhs) {
+    /* Transform integer to fraction fith denum is equal to one, returns fraction */
+
+    return BigFra(lhs, BigInt("1"));
+}
+
+BigInt TRANS_Q_Z(BigFra lhs) {
+	/* If possible, transform fraction to integer returns result, else returns minus zero*/
+	
+    if(INT_Q_B(lhs) == true){
+        return lhs.numerator / lhs.denumenator;
     }
+    return BigInt("-0");
+}
+
+BigFra MUL_QQ_Q(BigFra lhs, BigFra rhs) {
+    /* Miltiply lhs to rhs, returns result */
+
+    lhs.numerator = (MUL_ZZ_Z(lhs.numerator, rhs.numerator));
+    rhs.denominator = (MUL_ZZ_Z(lhs.denominator, rhs.denominator));
+    
+    return RED_Q_Q(BigFra(lhs.numerator,rhs.denominator));
+}
+
+BigFra DIV_QQ_Q(BigFra lhs, BigFra rhs) {
+    /* Divede lhs to rhs, returns result */
+	
+	assert(("DIV_QQ_Q rhs is zero", rhs.numerator != BigInt("0"));
+
+    lhs.numerator = lhs.numerator * rhs.denominator;
+    rhs.denominator = lhs.denominator * rhs.numerator;
+	
+
+    return RED_Q_Q(BigFra(lhs.numerator,rhs.denominator));
 }
 
 BigFra RED_Q_Q(BigFra lhs) {
-    BigFra temp = lhs;
+	/* Makes fraction irrreducable, returns new fraction */
 	
+    BigFra temp = lhs;
+
     BigInt gcd = BigInt(GCD_NN_N(temp.numerator, temp.denominator), false);
 
     temp.numerator = temp.numerator / gcd;
@@ -48,23 +69,10 @@ BigFra RED_Q_Q(BigFra lhs) {
 
     return temp;
 }
-/*
-BigFra ADD_QQ_Q(BigFra lhs, BigFra rhs) {
-
-    BigInt lcm(LCM_NN_N(lhs.denominator, rhs.denominator), false);
-
-    lhs.numerator = lhs.numerator * lcm / lhs.denominator;
-    lhs.denominator = lcm;
-
-    rhs.numerator = rhs.numerator * lcm / rhs.denominator;
-    rhs.denominator = lcm;
-
-    BigFra temp(lhs.numerator + rhs.numerator, lcm);
-
-    return RED_Q_Q(temp);
-}*/
 
 BigFra ADD_QQ_Q(BigFra lhs, BigFra rhs) {
+	/* Substract add rhs to lhs, returns result */
+	
     BigInt lcm = lhs.denominator * rhs.denominator;
 
     lhs.numerator = lhs.numerator * rhs.denominator;
@@ -76,53 +84,23 @@ BigFra ADD_QQ_Q(BigFra lhs, BigFra rhs) {
 }
 
 BigFra SUB_QQ_Q(BigFra lhs, BigFra rhs) {
-	//USE ADD
+    /* Substract rhs from lhs, returns result */
+    
     rhs.numerator = MUL_ZM_Z(rhs.numerator);
 
     return ADD_QQ_Q(lhs, rhs);
 }
 
-bool INT_Q_B(BigInt lhs, BigInt rhs) {
-    /* Checks if fraction is integer */
-
-    return (lhs % rhs == BigInt("0"));
-}
-
-BigFra TRANS_Z_Q(BigInt lhs) {
-    /* Transform integer to fraction fith denum is equal to one, returns fraction */
-
-    BigInt denum("1");
-    //std::cout << "forsacen: " << lhs << "  " << denum << std::endl;
-    return BigFra(lhs, denum);
-}
-
-BigInt TRANS_Q_Z(BigFra lhs) {
-
-    if(lhs.denominator == BigInt("1")){
-        return lhs.numerator;
+void BigFra::normalise(){
+    int isNegative1 = POZ_Z_D(numerator);
+    int isNegative2 = POZ_Z_D(denominator);
+    if(isNegative1 == isNegative2){
+        numerator = BigInt(numerator, false);
+        denominator = BigInt(denominator, false);
+    }else{
+        numerator = BigInt(numerator, true);
+        denominator = BigInt(denominator, false);
     }
-    return BigInt("0");
-}
-
-BigFra MUL_QQ_Q(BigFra lhs, BigFra rhs) {
-    /* */
-
-    lhs.numerator = (MUL_ZZ_Z(lhs.numerator, rhs.numerator));
-    rhs.denominator = (MUL_ZZ_Z(lhs.denominator, rhs.denominator));
-    
-    return BigFra(lhs.numerator,rhs.denominator);
-}
-
-BigFra DIV_QQ_Q(BigFra lhs, BigFra rhs) {
-    /* */
-
-    if(rhs.numerator == BigInt("0")){
-        //Here isn't error message
-    } else {
-        lhs.numerator = (MUL_ZZ_Z(lhs.numerator, rhs.denominator));
-        rhs.denominator = (MUL_ZZ_Z(lhs.denominator, rhs.numerator));
-    }
-    return BigFra(lhs.numerator,rhs.denominator);
 }
 
 //==OPERATORS===
@@ -142,9 +120,4 @@ BigFra BigFra::operator*(const BigFra &rhs){
 BigFra BigFra::operator/(const BigFra &rhs){
     return DIV_QQ_Q(*this, rhs);
 }
-
-
-
-
-
 
--- include/big_q.h
+++ include/big_q.h
@@ -10,30 +10,29 @@ class BigFra{
 public:
 	friend std::ostream& operator<< (std::ostream &stream, BigFra num);
 
-    //=====MODULES=====
-    //Author: Alex Kosh 7301
-    //Problems witr cout function return
+	//=====MODULES=====
+	//Author: Demid Tremp 7301
 
-    friend BigFra RED_Q_Q(BigFra lhs); //cheched c:
-    friend bool INT_Q_B(BigInt lhs, BigInt rhs); //checked c:
+    friend bool INT_Q_B(BigInt lhs, BigInt rhs);
 	friend BigFra TRANS_Z_Q(BigInt lhs);
 	friend BigInt TRANS_Q_Z(BigFra lhs);
-    friend BigFra ADD_QQ_Q(BigFra lhs, BigFra rhs);
-    friend BigFra SUB_QQ_Q(BigFra lhs, BigFra rhs);
     friend BigFra MUL_QQ_Q(BigFra lhs, BigFra rhs);
     friend BigFra DIV_QQ_Q(BigFra lhs, BigFra rhs);
 
-    friend void toString(std::ostream &stream, BigFra lhs, int digits);
-    void normalise();
-    
+	//Author: Stepan Fed
+	friend BigFra RED_Q_Q(BigFra lhs);
+	friend BigFra ADD_QQ_Q(BigFra lhs, BigFra rhs);
+	friend BigFra SUB_QQ_Q(BigFra lhs, BigFra rhs);
+
+	//======Operators support======
     BigFra operator+(const BigFra &rhs);
     BigFra operator-(const BigFra &rhs);
     BigFra operator/(const BigFra &rhs);
     BigFra operator*(const BigFra &rhs);
-    
-	BigFra(BigInt numenator, BigInt denuminator);
-	BigFra(std::string str);
 
+	//===================
+	BigFra(BigInt numenator, BigInt denuminator);
+	void normalise();
 };
 
 #endif
